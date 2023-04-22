8e13d359937547778c47dca2630c28eaafcea066
Xing Zhou <xing@xingzhou.me>
2023-04-21 22:01:09 -0400

    refactor: make component tests more readable

--- cypress/component/AsyncScript.cy.tsx
+++ cypress/component/AsyncScript.cy.tsx
@@ -2,47 +2,63 @@ import AsyncScript from "@/components/AsyncScript";
 
 describe('AsyncScript', () => {
     const src = "/js/hello-world.js";
-    beforeEach(() => {
-        cy.intercept(src, '').as('LoadingScript')
-    })
+
+    function mount(component: JSX.Element,
+                   configureLoadingScript = () => {
+                       cy.intercept(src, '').as('LoadingScript')
+                   }
+    ) {
+        configureLoadingScript()
+        cy.mount(component)
+        cy.wait('@LoadingScript')
+    }
 
     it('loads script at the specified src', () => {
-        cy.mount(<AsyncScript src={src}/>)
-        cy.wait('@LoadingScript')
+        mount(<AsyncScript src={src}/>)
         cy.get(`[src="${src}"]`).should('have.length', 1)
     });
 
     it('calls the onLoad prop once the script loads', () => {
-        cy.mount(<AsyncScript src={src} onLoad={cy.spy().as('OnLoadSpy')}/>)
-        cy.wait('@LoadingScript')
+        mount(<AsyncScript src={src} onLoad={cy.spy().as('OnLoadSpy')}/>)
         cy.get('@OnLoadSpy').should('have.been.called')
     });
 
     it('calls the onError prop if the script fails to load', () => {
-        cy.intercept(src, {statusCode: 404}).as('LoadingScript')
-        cy.mount(<AsyncScript src={src} onLoad={cy.spy().as('OnLoadSpy')} onError={cy.spy().as('OnErrorSpy')}/>)
-        cy.wait('@LoadingScript')
+        mount(
+            <AsyncScript
+                src={src}
+                onLoad={cy.spy().as('OnLoadSpy')}
+                onError={cy.spy().as('OnErrorSpy')}
+            />,
+            () => {
+                cy.intercept(src, {statusCode: 404}).as('LoadingScript')
+            }
+        )
         cy.get('@OnErrorSpy').should('have.been.called')
         cy.get('@OnLoadSpy').should('not.have.been.called')
     });
 
-    it('appends the script to the node specified by the appendTo prop', () => {
-        cy.document().then((doc) => {
-            const element = doc.createElement('div');
-            element.setAttribute('data-test-id', 'app-mount-point')
-            doc.body.append(element)
-            return cy.wrap(element)
-        }).as('MountPoint')
-        cy.get('@MountPoint').then(($element) => {
-            cy.mount(<AsyncScript src={src} appendTo={$element.get()[0]}/>)
-        })
-        cy.wait('@LoadingScript')
-        cy.get('@MountPoint').children().should('have.length.at.least', 1)
-    });
+    describe('Appending script tag to HTML proper element', () => {
+        it('appends to appendTo prop value', () => {
+            function createElementToAppendTo() {
+                return cy.document().then((doc) => {
+                    const element = doc.createElement('div');
+                    element.setAttribute('data-test-id', 'app-mount-point')
+                    doc.body.append(element)
+                    return cy.wrap(element)
+                })
+            }
 
-    it('appends the script document.head if appendTo prop is not set', () => {
-        cy.mount(<AsyncScript src={src}/>)
-        cy.wait('@LoadingScript')
-        cy.get('head').find(`script[src="${src}"]`).should('exist')
+            createElementToAppendTo().as('MountPoint')
+            cy.get('@MountPoint').then(($element) => {
+                mount(<AsyncScript src={src} appendTo={$element.get(0)}/>)
+            })
+            cy.get('@MountPoint').children().should('have.length.at.least', 1)
+        });
+
+        it('appends to document.head if appendTo prop is not set', () => {
+            mount(<AsyncScript src={src}/>)
+            cy.get('head').find(`script[src="${src}"]`).should('exist')
+        });
     });
 })
